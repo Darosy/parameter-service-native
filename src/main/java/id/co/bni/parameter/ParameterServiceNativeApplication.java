ba4de7eb3363a406ac1477668963dae44d99e9bb
darosy.imam <darosy.imam@gmail.com>
2023-12-27 15:12:18 +0700

    initial

--- src/main/java/id/co/bni/parameter/ParameterServiceNativeApplication.java
+++ src/main/java/id/co/bni/parameter/ParameterServiceNativeApplication.java
@@ -2,27 +2,12 @@ package id.co.bni.parameter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
-@EnableAsync
 public class ParameterServiceNativeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ParameterServiceNativeApplication.class, args);
     }
 
-    @Bean
-    public TaskExecutor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(2);
-        executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("Async-Loader-");
-        executor.initialize();
-        return executor;
-    }
 }
---
+++ src/main/java/id/co/bni/parameter/config/AsyncConfig.java
@@ -0,0 +1,22 @@
+package id.co.bni.parameter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("Async-Loader-");
+        executor.initialize();
+        return executor;
+    }
+}
